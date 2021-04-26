cafa303783f72f6af1fbbdf92048d9a57f3d1e6c
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2021-04-26 18:33:31 -0300

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <p>Mi cumpleaños</p>
-      <Faltan date = "2020/03/24" />
+      <Faltan date = "2020/03/22" />
       <p>Días que han pasado</p>
       <Faltan date = "2020/01/01"/>
     </div>
